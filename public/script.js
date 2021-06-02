9428a1e92dfba3f6d869dc6f51cad892473f0de8
jeff14994 <jeff14994@gmail.com>
2021-06-02 16:12:15 +0800

    Complete find the location of user

--- public/script.js
+++ public/script.js
@@ -2,9 +2,10 @@
 // Create a map object
 // Set up the longitude and latitude
 // Set up zoom size
-const map = L.map('map', {
-  center: [24.144, 120.6844],
-  zoom: 16,
+
+const map = L.map('map').locate({ 
+  setView: true,
+  maxZoom: 16,
 });
 // eslint-disable-next-line no-undef
 // const map = L.map('map', {
@@ -45,3 +46,36 @@ xhr.onload = () => {
   }
   map.addLayer(markers);
 };
+// Get location
+function geoFindMe() {
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // console.log(latitude, longitude);
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    return (latitude, longitude);
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if (!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
+geoFindMe();

5351eb112482d02ded6c976b7b42788ae27452bc
jeff14994 <jeff14994@gmail.com>
2021-06-02 17:30:27 +0800

    Complete pinpoint user location with red marker

--- public/script.js
+++ public/script.js
@@ -3,10 +3,12 @@
 // Set up the longitude and latitude
 // Set up zoom size
 
-const map = L.map('map').locate({ 
-  setView: true,
-  maxZoom: 16,
-});
+// Ask user's location
+const map = L.map('map')
+  .locate({
+    setView: true,
+    maxZoom: 16,
+  });
 // eslint-disable-next-line no-undef
 // const map = L.map('map', {
 //   center: [22.604799, 120.2976256],
@@ -18,6 +20,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+// The style of the current location icon
+const redIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+// The style of the store's icon
 const greenIcon = new L.Icon({
   iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -26,6 +38,19 @@ const greenIcon = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
+
+// Pinpoint user's current location
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    latit = position.coords.latitude;
+    longit = position.coords.longitude;
+    // this is just a marker placed in that position
+    // Use .openPopup() to open the popup immediately;
+    L.marker([position.coords.latitude, position.coords.longitude], { icon: redIcon }).addTo(map).bindPopup('<h2>Current Location!</h2>').openPopup();
+    // move the map to have the location in its center
+    map.panTo(new L.LatLng(latit, longit));
+  });
+}
 
 const markers = new L.MarkerClusterGroup().addTo(map);
 
@@ -46,36 +71,53 @@ xhr.onload = () => {
   }
   map.addLayer(markers);
 };
-// Get location
-function geoFindMe() {
-  const status = document.querySelector('#status');
-  const mapLink = document.querySelector('#map-link');
+// const popup = L.popup();
 
-  mapLink.href = '';
-  mapLink.textContent = '';
-
-  function success(position) {
-    const { latitude } = position.coords;
-    const { longitude } = position.coords;
-    // console.log(latitude, longitude);
-
-    status.textContent = '';
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    return (latitude, longitude);
-  }
-
-  function error() {
-    status.textContent = 'Unable to retrieve your location';
-  }
-
-  if (!navigator.geolocation) {
-    status.textContent = 'Geolocation is not supported by your browser';
-  } else {
-    status.textContent = 'Locating…';
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent(`經緯度座標：${e.latlng.toString()}`)
+    .openOn(map);
+    console.log(e.latlng.lat);
+    console.log(e.latlng.lng);
 }
+// function onMapClick(e) {
+//   alert("經緯度座標：" + e.latlng);
+// }
 
-document.querySelector('#find-me').addEventListener('click', geoFindMe);
-geoFindMe();
+map.on('click', onMapClick);
+
+map.on('click', onMapClick);
+// Get location
+// function geoFindMe() {
+//   const status = document.querySelector('#status');
+//   const mapLink = document.querySelector('#map-link');
+
+//   mapLink.href = '';
+//   mapLink.textContent = '';
+
+//   function success(position) {
+//     const { latitude } = position.coords;
+//     const { longitude } = position.coords;
+//     // console.log(latitude, longitude);
+
+//     status.textContent = '';
+//     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+//     mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+//     return (latitude, longitude);
+//   }
+
+//   function error() {
+//     status.textContent = 'Unable to retrieve your location';
+//   }
+
+//   if (!navigator.geolocation) {
+//     status.textContent = 'Geolocation is not supported by your browser';
+//   } else {
+//     status.textContent = 'Locating…';
+//     navigator.geolocation.getCurrentPosition(success, error);
+//   }
+// }
+
+// document.querySelector('#find-me').addEventListener('click', geoFindMe);
+// geoFindMe();
